2fb2f657a89907a41df652bae7c1729e5a873945
Yoko <mi_yoko24555@outlook.jp>
2023-10-01 13:24:51 +0900

    5th commit

--- src/app.py
+++ src/app.py
@@ -13,19 +13,11 @@ import io
 from PIL import Image
 import base64
 from loguru import logger
-
 from build import get_exp
+import tempfile
 
 # 設定ファイルのパス
 config_path = './src/yolox_s_coin.py'
-
-# 設定ファイルを読み込む
-#exp = get_exp(config_path)
-
-#　推論した合計金額を返す関数
-#def getName(output):
-#    logger.info(f"c_app.py : getName {output}")
-#    return output
 
 # Flask のインスタンスを作成
 app = Flask(__name__)
@@ -43,7 +35,7 @@ def predicts():
     # リクエストがポストかどうかの判別
     if request.method == 'POST':
         # ファイルがなかった場合の処理
-        logger.info(f"c_app.py : filename = {'filename'}")
+#        logger.info(f"app.py : filename = {filename}")
         if 'filename' not in request.files:
             return redirect(request.url)
         # データの取り出し
@@ -53,35 +45,43 @@ def predicts():
         if file and allowed_file(file.filename):
 
             #　画像ファイルに対する処理
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                file.save(temp_file)
+                temp_file_input_path = temp_file.name
+                logger.info(f"temp_file_input_path: {temp_file_input_path}")
+
             #　画像書き込み用バッファを確保
             buf = io.BytesIO()
             image = Image.open(file).convert('RGB')
             #　画像データをバッファに書き込む
             image.save(buf, 'png')
             #　画像データをファイルに書き込む
-            image.save('./datafolder/saved_image.jpg')
+#            image.save('./datafolder/saved_image.jpg')
             #画像が読み込まれているか
-            logger.info(f"Image width: {image.width}, height: {image.height}")
+#            logger.info(f"Image width: {image.width}, height: {image.height}")
             #　バイナリデータを base64 でエンコードして utf-8 でデコード
             base64_str = base64.b64encode(buf.getvalue()).decode('utf-8')
             #　HTML 側の src の記述に合わせるために付帯情報付与する
             base64_data = 'data:image/png;base64,{}'.format(base64_str)
                         
-            parser = argparse.ArgumentParser("YOLOX Demo!")
-            parser.add_argument("demo", default="image", help="demo type, eg. image, video and webcam")
-            parser.add_argument("-f", "--exp_file", default=config_path, type=str, help="please input your experiment description file")
-            parser.add_argument("-c", "--ckpt", default="./src/best_ckpt.pth", type=str, help="ckpt for eval")
-            parser.add_argument("--device", default="cpu", type=str, help="device to run our model, can either be cpu or gpu")
-            parser.add_argument("--path", default='./datafolder/saved_image.jpg', help="path to images or video")
-            parser.add_argument("--save_result", action="store_true", help="whether to save the inference result of image/video")
+#            parser = argparse.ArgumentParser("YOLOX Demo!")
+#            parser.add_argument("demo", default="image", help="demo type, eg. image, video and webcam")
+#            parser.add_argument("-f", "--exp_file", default=config_path, type=str, help="please input your experiment description file")
+#            parser.add_argument("-c", "--ckpt", default="./src/best_ckpt.pth", type=str, help="ckpt for eval")
+#            parser.add_argument("--device", default="cpu", type=str, help="device to run our model, can either be cpu or gpu")
+#            parser.add_argument("--path", default=temp_file_input_path, help="path to images or video")
+#            parser.add_argument("--save_result", action="store_true", help="whether to save the inference result of image/video")
 
             args = make_parser().parse_args()
-            logger.info(f"c_app.py : config_path = {config_path}, agrs.demo = {args.demo}")
-            logger.info(f"c_app.py : config_path = {config_path}, agrs.save_result C = {args.save_result}")
+            args.path = temp_file_input_path
+
+            logger.info(f"app.py : config_path = {config_path}, args.demo = {args.demo}")
+            logger.info(f"args.--path = {args.path}")            
+            logger.info(f"args.save_result C = {args.save_result}")
             exp = get_exp(config_path, args.demo)
 
             coinTotal_, save_file_name = main(exp, args)
-            logger.info(f"c_app.py : save_file_name = {save_file_name}")
+            logger.info(f"app.py : save_file_name = {save_file_name}")
 
             with open(save_file_name, "rb") as image_file_result:
                 #　バイナリデータを base64 でエンコードして utf-8 でデコード
--- src/demo.py
+++ src/demo.py
@@ -16,6 +16,8 @@ from build import get_exp
 from model_utils import fuse_model, get_model_info
 from boxes import postprocess
 from visualize import vis
+import tempfile
+
 IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
 
 
@@ -228,56 +230,63 @@ def image_demo(predictor, vis_folder, path, current_time, save_result):
 #####
 
         if save_result:
-            save_folder = vis_folder
+#            save_folder = vis_folder
 #            save_folder = os.path.join(
 #                vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
 #            )
-            logger.info(f"save_folder: {save_folder}")
-            os.makedirs(save_folder, exist_ok=True)
-            save_file_name = os.path.join(save_folder, os.path.basename(image_name))
-            logger.info("Saving detection result in {}".format(save_file_name))
-            cv2.imwrite(save_file_name, result_image)
-        ch = cv2.waitKey(0)
-        if ch == 27 or ch == ord("q") or ch == ord("Q"):
-            break
+#            logger.info(f"save_folder: {save_folder}")
+#            os.makedirs(save_folder, exist_ok=True)
+            with tempfile.NamedTemporaryFile(delete=False,suffix=".png") as temp_file_result:
+                cv2.imwrite(temp_file_result.name, result_image) 
+#                outputs.save(temp_file_result)
+                temp_file_result_path = temp_file_result.name
+            logger.info(f"temp_file_result_path: {temp_file_result_path}")
+
+#            save_file_name = os.path.join(save_folder, os.path.basename(image_name))
+#            logger.info("Saving detection result in {}".format(save_file_name))
+#            cv2.imwrite(save_file_name, result_image)
+#        ch = cv2.waitKey(0)
+#        if ch == 27 or ch == ord("q") or ch == ord("Q"):
+#            break
     
-    logger.info("image_demo save_file_name : {}".format(save_file_name))
-    return total_amount, save_file_name
+#    logger.info("image_demo save_file_name : {}".format(save_file_name))
+#    return total_amount, save_file_name
+    return total_amount, temp_file_result_path
 
 
-def imageflow_demo(predictor, vis_folder, current_time, args):
-    cap = cv2.VideoCapture(args.path if args.demo == "video" else args.camid)
-    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    if args.save_result:
-        save_folder = os.path.join(
-            vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
-        )
-        os.makedirs(save_folder, exist_ok=True)
-        if args.demo == "video":
-            save_path = os.path.join(save_folder, os.path.basename(args.path))
-        else:
-            save_path = os.path.join(save_folder, "camera.mp4")
-        logger.info(f"video save_path is {save_path}")
-        vid_writer = cv2.VideoWriter(
-            save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height))
-        )
-    while True:
-        ret_val, frame = cap.read()
-        if ret_val:
-            outputs, img_info = predictor.inference(frame)
-            result_frame = predictor.visual(outputs[0], img_info, predictor.confthre)
-            if args.save_result:
-                vid_writer.write(result_frame)
-            else:
-                cv2.namedWindow("yolox", cv2.WINDOW_NORMAL)
-                cv2.imshow("yolox", result_frame)
-            ch = cv2.waitKey(1)
-            if ch == 27 or ch == ord("q") or ch == ord("Q"):
-                break
-        else:
-            break
+#def imageflow_demo(predictor, vis_folder, current_time, args):
+#    cap = cv2.VideoCapture(args.path if args.demo == "video" else args.camid)
+#    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+#    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+#    fps = cap.get(cv2.CAP_PROP_FPS)
+#    if args.save_result:
+#        save_folder = os.path.join(
+#            vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
+#        )
+#        os.makedirs(save_folder, exist_ok=True)
+#        if args.demo == "video":
+#            save_path = os.path.join(save_folder, os.path.basename(args.path))
+#        else:
+#            save_path = os.path.join(save_folder, "camera.mp4")
+#        logger.info(f"video save_path is {save_path}")
+#        vid_writer = cv2.VideoWriter(
+#            save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height))
+#        )
+#    while True:
+#        ret_val, frame = cap.read()
+#        if ret_val:
+#            outputs, img_info = predictor.inference(frame)
+#            result_frame = predictor.visual(outputs[0], img_info, predictor.confthre)
+#            if args.save_result:
+#                vid_writer.write(result_frame)
+#            else:
+#                cv2.namedWindow("yolox", cv2.WINDOW_NORMAL)
+#                cv2.imshow("yolox", result_frame)
+#            ch = cv2.waitKey(1)
+#            if ch == 27 or ch == ord("q") or ch == ord("Q"):
+#                break
+#        else:
+#            break
 
 
 def main(exp, args):
